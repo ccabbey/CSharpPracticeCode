5ee6c3f8edd7f0ef1138a407d194f937effc73b5
AlexLi <ccabbey@126.com>
2023-02-11 00:17:52 +0800

    add ch11ex04 demo

---
+++ CH11/Ex04_IsOperatorDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using static System.Console;
+
+namespace Ch11Ex04
+{
+
+
+    public class Checker
+    {
+        public void Check(object param1)
+        {
+            if (param1 is ClassA)
+                WriteLine($"Variable can be converted to ClassA");
+            else
+                WriteLine($"Variable can't be converted to ClassA");
+            if (param1 is IMyInterface)
+                WriteLine($"Variable can be converted to IMyInterface");
+            else
+                WriteLine($"Variable can't be converted to IMyInterface");
+            if (param1 is MyStruct)
+                WriteLine($"Variable can be converted to MyStruct");
+            else
+                WriteLine($"Variable can't be converted to MyStruct");
+        }
+    }
+    interface IMyInterface { }
+
+    class ClassA : IMyInterface { }
+
+    class ClassB : IMyInterface { }
+
+    class ClassC { }
+
+    class ClassD : ClassA { }
+
+    public struct MyStruct { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Checker checker = new();
+            ClassA try1 = new();
+            ClassB try2 = new();
+            ClassC try3 = new();
+            ClassD try4 = new();
+            MyStruct try5 = new();
+            object try6 = try5;
+
+            WriteLine("Analyzing ClassA type variable:");
+            checker.Check(try1);
+            WriteLine("AnAnalyzing ClassB type variable:");
+            checker.Check(try2);
+            WriteLine("AnAnalyzing ClassC type variable:");
+            checker.Check(try3);
+            WriteLine("Analyzing ClassD type variable:");
+            checker.Check(try4);
+            WriteLine("Analyzing MyStruct type variable:");
+            checker.Check(try5);
+            WriteLine("Analyzing boxed MyStruct type variable:");
+            checker.Check(try6);
+            ReadKey();
+        }
+    }
+
+
+}
