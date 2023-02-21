0e9415dde8663a2030c5dfad2a025a22a73c2083
AlexLi <ccabbey@126.com>
2023-02-21 22:21:01 +0800

    new: add AS operator demo

---
+++ CH11/11.3.2_AsOperatorDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+namespace Ch11AsOperatorDemo
+{
+
+    class ClassA : IMyInterface { }
+
+    internal interface IMyInterface
+    {
+    }
+
+    class ClassD : ClassA { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA obj1 = new();
+            ClassD? obj2 = obj1 as ClassD;
+            Console.WriteLine(obj2 is null);
+            Console.ReadKey();
+        }
+    }
+
+
+}
