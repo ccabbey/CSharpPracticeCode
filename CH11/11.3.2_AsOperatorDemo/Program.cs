63d87a18eec9385957ca310608da3d6ea38e6879
ccabbey <ccabbey@126.com>
2023-02-22 19:47:03 +0800

    new: add AS operator demo

--- CH11/11.3.2_AsOperatorDemo/Program.cs
+++ CH11/11.3.2_AsOperatorDemo/Program.cs
@@ -15,9 +15,12 @@ namespace Ch11AsOperatorDemo
     {
         static void Main(string[] args)
         {
-            ClassA obj1 = new();
-            ClassD? obj2 = obj1 as ClassD;
-            Console.WriteLine(obj2 is null);
+
+            //子类可以隐式转换为其基类，
+            ClassD obj1 = new();
+            ClassA? obj2 = obj1 as ClassA;  //obj2 will be null
+            Console.WriteLine($"obj2 is null? {obj2 is null}, type of obj2 is {obj2?.GetType().ToString()}");
+            // Console.WriteLine(typeof(ClassD));
             Console.ReadKey();
         }
     }
