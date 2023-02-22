0ea85fc1ad314dcbe1d4a074e0a425174a76aa98
AlexLi <ccabbey@126.com>
2023-02-22 23:22:38 +0800

    new: add AS operator demo

--- CH11/11.3.2_AsOperatorDemo/Program.cs
+++ CH11/11.3.2_AsOperatorDemo/Program.cs
@@ -15,13 +15,37 @@ namespace Ch11AsOperatorDemo
     {
         static void Main(string[] args)
         {
+            //使用AS运算符进行类型转换
 
-            //子类可以隐式转换为其基类，
-            ClassD obj1 = new();
-            ClassA? obj2 = obj1 as ClassA;  //obj2 will be null
-            Console.WriteLine($"obj2 is null? {obj2 is null}, type of obj2 is {obj2?.GetType().ToString()}");
-            // Console.WriteLine(typeof(ClassD));
+            //子类可以隐式转换为其基类，但基类不可隐式转换为子类
+            ClassA obj1 = new();
+            ClassD? obj2 = obj1 as ClassD;  //obj2 will be null
+            if (obj2 is null)
+            {
+                Console.WriteLine("obj2 is null");
+            }
+            else
+            {
+                Console.WriteLine($"obj2 is {obj2.GetType()}");
+            }
+
+
+            ClassD obj3 = new();
+            ClassA? obj4 = obj3 as ClassA;  //obj4 will be instance of ClassA(parent)
+            if (obj4 is null)
+            {
+                Console.WriteLine("obj4 is null");
+            }
+            else
+            {
+                Console.WriteLine($"obj4 is {obj4.GetType()}");
+            }
+
             Console.ReadKey();
+
+            //使用多态性
+
+
         }
     }
 
